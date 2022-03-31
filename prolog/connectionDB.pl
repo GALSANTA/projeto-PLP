3f9de23504a8e39ab60a0f9b55230ecdde14479c
Anderson Melo <anderson.melo@ccc.ufcg.edu.br>
2022-03-31 16:26:19 +0000

    FEAT: Criando cadastro de usuario

--- prolog/connectionDB.pl
+++ prolog/connectionDB.pl
@@ -1,17 +1,30 @@
-create_connection :-
+:- use_module(library(odbc)).
+
+create_conn :-
     odbc_connect('projetoPLP', _,
-                    [ user(remote),
-                    password(123),
-                    alias(conn),
+                    [ user('remote'),
+                    password('123'),
+                    alias('conn'),
                     open(once)
                     ]).
 
 drop_connection :-
     odbc_disconnect(conn).
 
-cadastra(Nome, Cpf, Matricula, Profissao, Usuario, Senha, F) :-
+cadastra_usuario(F) :-
+    write('Digite seu nome: '), nl,
+    read(Nome),
+    write('Digite seu cpf: '), nl,
+    read(Cpf),
+    write('Digite sua Matricula: '), nl,
+    read(Matricula),
+    write('Digite sua Profissão: '), nl,
+    read(Profissao),
+    write('Digite seu Usuario: '), nl,
+    read(Usuario),
+    write('Digite sua Senha: '), nl,
+    read(Senha),
     odbc_query('conn',
-                'INSERT INTO tb_usuario (id_usuario, nome, cpf, matricula, profissao, usuario, senha)
-                VALUES (?, Nome, Cpf, Matricula, Profissao, Usuario, Senha, F)',
+                "INSERT INTO tb_usuario (nome, cpf, matricula, profissao, usuario, senha)
+                 VALUES ('~w', '~w', '~w', '~w', '~w', '~w')"-[Nome, Cpf, Matricula, Profissao, Usuario, Senha],
                 affected(F)).
-
